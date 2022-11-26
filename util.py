bfbed5efde59b4065b24983fc8ebb412787e1e74
zhliuworks <zihanliu2000@gmail.com>
2022-11-26 12:51:15 +0800

    add basic utils

---
+++ util.py
@@ -0,0 +1,101 @@
+import os
+import tqdm
+import logging
+import torch
+
+
+def get_save_dir(base_dir, name, training, id_max=100):
+    """Get a unique save directory by appending the smallest positive integer
+    `id < id_max` that is not already taken (i.e., no dir exists with that id).
+
+    Args:
+        base_dir (str): Base directory in which to make save directories.
+        name (str): Name to identify this training run. Need not be unique.
+        training (bool): Save dir. is for training (determines subdirectory).
+        id_max (int): Maximum ID number before raising an exception.
+
+    Returns:
+        save_dir (str): Path to a new directory with a unique name.
+    """
+    for uid in range(1, id_max):
+        subdir = 'train' if training else 'test'
+        save_dir = os.path.join(base_dir, subdir, f'{name}-{uid:02d}')
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+            return save_dir
+
+    raise RuntimeError('Too many save directories created with the same name. \
+                       Delete old save directories or use another name.')
+
+
+def get_logger(log_dir, name):
+    """Get a `logging.Logger` instance that prints to the console
+    and an auxiliary file.
+
+    Args:
+        log_dir (str): Directory in which to create the log file.
+        name (str): Name to identify the logs.
+
+    Returns:
+        logger (logging.Logger): Logger instance for logging events.
+    """
+    class StreamHandlerWithTQDM(logging.Handler):
+        """Let `logging` print without breaking `tqdm` progress bars.
+
+        See Also:
+            > https://stackoverflow.com/questions/38543506
+        """
+        def emit(self, record):
+            try:
+                msg = self.format(record)
+                tqdm.tqdm.write(msg)
+                self.flush()
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                self.handleError(record)
+
+    # Create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # Log everything (i.e., DEBUG level and above) to a file
+    log_path = os.path.join(log_dir, 'log.txt')
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setLevel(logging.DEBUG)
+
+    # Log everything except DEBUG level (i.e., INFO level and above) to console
+    console_handler = StreamHandlerWithTQDM()
+    console_handler.setLevel(logging.INFO)
+
+    # Create format for the logs
+    file_formatter = logging.Formatter('[%(asctime)s] %(message)s',
+                                       datefmt='%m.%d.%y %H:%M:%S')
+    file_handler.setFormatter(file_formatter)
+    console_formatter = logging.Formatter('[%(asctime)s] %(message)s',
+                                          datefmt='%m.%d.%y %H:%M:%S')
+    console_handler.setFormatter(console_formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+def get_available_devices():
+    """Get IDs of all available GPUs.
+
+    Returns:
+        device (torch.device): Main device (GPU 0 or CPU).
+        gpu_ids (list): List of IDs of all GPUs that are available.
+    """
+    gpu_ids = []
+    if torch.cuda.is_available():
+        gpu_ids += [gpu_id for gpu_id in range(torch.cuda.device_count())]
+        device = torch.device(f'cuda:{gpu_ids[0]}')
+        torch.cuda.set_device(device)
+    else:
+        device = torch.device('cpu')
+
+    return device, gpu_ids
